22ac5e035b6c874143e04ae9ebe30104c1360bf3
archstation <ejovo13@yahoo.com>
2022-11-19 18:50:03 +0100

    add openmpi support

--- test/test_omp_proc_available.c
+++ test/test_omp_proc_available.c
@@ -1,18 +1,30 @@
 #include "geodesy.h"
 #include "omp.h"
+#include "mpi.h"
 
 // Let's see how many threads are available on this machine
 
-int main() {
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv){
+
+    int this_rank, world_size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
 
     #pragma omp parallel
     {
 
         if (omp_get_thread_num() == 0) {
-            printf("Num available threads: %d\n", omp_get_num_threads());
+            printf("Node %d has available threads: %d\n", this_rank, omp_get_num_threads());
         }
 
     }
+
+    MPI_Finalize();
 
     return 0;
 }
